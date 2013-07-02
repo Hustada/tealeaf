a43089bc4112de9ea75e192e809b7b021a5c5b02
Hustada <hustada80@gmail.com>
2013-07-02 14:37:42 -0500

    modified

--- calcexercise.rb
+++ calcexercise.rb
@@ -5,17 +5,19 @@ puts "What is the second number?"
 num2 = gets.chomp
 
 puts "What would you like to do? 1)Add 2)Subtract 3)Multiply 4)Divide"
-operation = gets.chomp
-
 arr = ['1','2','3','4'] 
-if operation = "1"
-  result = num1.to_s + num2.to_s
-elsif operation = "2"
-  result = num1.to_s - num2.to_s
-elsif operation = "3"
-  result = num1.to_s * num2.to_s
-else operation = "4"
-  result = num1.to_i / num2.to_i
+result = gets.chomp
+
+
+
+if operation = arr[0]
+  result == num1.to_s + num2.to_s
+elsif operation = arr[1]
+  result == num1.to_s - num2.to_s
+elsif operation = arr[2]
+  result == num1.to_s * num2.to_s
+else operation = arr[4]
+  result == num1.to_i / num2.to_i
 end   
 
 puts 'Answer is ' + result + ''     
